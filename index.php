442a9474eaf4c33e2913624db9e862fa30f35095
Fabian3830 <49417624+Fabian3830@users.noreply.github.com>
2019-07-31 04:35:28 -0600

    Mejora de interfaz

---
+++ index.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include("header.php") ?>
+
+<body>
+
+  <!-- Navigation -->
+
+  <?php include("navbar.php") ?>
+
+  <!-- Page Content -->
+  <div class="container-fluid">
+
+    <div class="row mx-auto">
+
+      <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+          <div class="carousel-item active">
+            <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+
+      <div class="row">
+
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Item One</a>
+              </h4>
+              <h5>$24.99</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Item Two</a>
+              </h4>
+              <h5>$24.99</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Item Three</a>
+              </h4>
+              <h5>$24.99</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Item Four</a>
+              </h4>
+              <h5>$24.99</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Item Five</a>
+              </h4>
+              <h5>$24.99</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">Item Six</a>
+              </h4>
+              <h5>$24.99</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <!-- /.row CARDS-->
+
+    </div>
+    <!-- /.row CONTENIDOS-->
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+
+  <?php include("footer.php") ?>
+
+  <!-- Footer -->
+
+  <!-- MODAL BUSQUEDA-->
+  <div class="modal fade" tabindex="-1" role="dialog" id="Busqueda" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="input-group flex-nowrap">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="addon-wrapping"><img src="img/lupa.png" class="img-fluid" alt="Responsive image" width="20"></span>
+          </div>
+          <input type="text" id="inputBuscar" placeholder="Buscar" aria-label="Username" aria-describedby="addon-wrapping">
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- /MODAL BUSQUEDA-->
+
+  <!-- MODAL SESION-->
+  <div class="modal fade" tabindex="-1" role="dialog" id="Sesion" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content" style="height: 350px">
+        <label class="mx-auto font-weight-bold mt-2" style="font-size: 22px">Inicia tu sesión!</label>
+        <form style="width:100%" class="mx-auto my-auto px-4">
+          <div class="form-group mx-auto">
+            <label for="InputEmail" style="font-size: 20px">Correo electrónico</label>
+            <input type="email" class="inputInicioSesion" id="InputEmail" aria-describedby="emailHelp" placeholder="Ingresa tu correo">
+          </div>
+          <div class="form-group mx-auto">
+            <label for="InputPassword" style="font-size: 20px">Contraseña</label>
+            <input type="password" class="inputInicioSesion" id="InputPassword" placeholder="Ingresa tu contraseña">
+          </div>
+          <div class="row">
+            <div class="col-auto">
+              <label class="mb-0" style="font-size: 14px">No te has registrado aún?</label><br />
+              <button type="button" class="btn btn-link text-danger btn-sm p-0">Registrate!</button>
+            </div>
+            <div class="col-auto ml-auto">
+              <button type="submit" class="btn btn-primary my-auto" style="width: 80px; height: 47px">
+                <label class="mb-0" style="font-size: 18px">Entrar</label>
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <!-- /MODAL SESION-->
+
+</body>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <a class="navbar-brand" href="#">TIENDA</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Camisas</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Figuras</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Posters</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Más
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Artistas</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Sobre Nosotros</a>
+          <a class="dropdown-item" href="#">Misión</a>
+          <a class="dropdown-item" href="#">Visión</a>
+          <a class="dropdown-item" href="#">Contáctanos</a>
+        </div>
+      </li>
+    </ul>
+
+    <button type="button" class="btn btn-primary mr-1" data-toggle="modal" data-target="#Busqueda">Buscar</button>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#Sesion">Iniciar Sesión</button>
+
+  </div>
+</nav>
