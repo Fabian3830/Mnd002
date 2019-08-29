19a5323b17eb41e33ff31319bea04bbce3d5e65f
JAFTSIX <42688559+JAFTSIX@users.noreply.github.com>
2019-08-29 13:42:47 -0600

    sd

--- index.php
+++ index.php
@@ -46,44 +46,61 @@
 
 
         <?php
-        $conn = oci_connect("Proyecto", "1", "localhost:1521/xe");
-        if (!$conn) {
-          $e = oci_error();
-          trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-        }
+ $conn = oci_connect("proyecto", "1", "localhost:1521/xe");
+ if (!$conn) {
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+    
+}
 
-        $sql= 'SELECT * FROM tb_ar';
-        $stid = oci_parse($conn,$sql);
+      
+        $retorno = array();
+        $retorno["valido"] = false;
+
+        $sql = "select * from TB_ARTICULO where nombre != :correo  ";
+
+
+
+
+        $stid = oci_parse($conn, $sql);
+
+        $a='1'  ;
+        oci_bind_by_name($stid, ':correo', $a);
         oci_execute($stid);
 
-     
+
         while (($row = oci_fetch_assoc($stid)) != false) {
-          $_SESSION["datos-usuario"] = array(
-              "Id" => $row['ID_CLIENTE'],
-              "correo" => $row['CORREO'],
-              "nombre" => $row['NOMBRE'],
-              "contrasena" => $row['CONTRA']
-          );
-          $retorno["valido"] = true;
-      }
+            
+            $retorno["valido"] = true;
+        }
+        
+
+       var_dump($retorno["valido"]);
+
+
+        /*while (($row = oci_fetch_assoc($stid)) != false) {
+     $JEJE =  '<div class="col-lg-4 col-md-6 mb-4">';
+     $JEJE .= '<div class="card h-100">';
+     $JEJE .=    '<a href="#"><img class="card-img-top" src="'.$row['IMG'].'" alt=""></a>';
+     $JEJE .=    '<div class="card-body">';
+     $JEJE .= '<h4 class="card-title">';
+     $JEJE .=   ' <a href="#">'.$row['NOMBRE'].'</a>';
+     $JEJE .=      '</h4>';
+     $JEJE .=  '<h5>$'.$row['PRECIO'].'</h5>';
+     $JEJE .=     ' <p class="card-text">'.$row['DESCRIP'].'</p>';
+     $JEJE .=   '</div>';
+     $JEJE .=    '<div class="card-footer">';
+     $JEJE .=   '   <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>';
+     $JEJE .= '  </div>';
+     $JEJE .= ' </div>';
+     $JEJE .=  '</div>';
+echo $JEJE ;
+
+        }*/
 
         ?>
 
-        <div class="col-lg-4 col-md-6 mb-4">
-          <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-            <div class="card-body">
-              <h4 class="card-title">
-                <a href="#">Item One</a>
-              </h4>
-              <h5>$24.99</h5>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-            </div>
-            <div class="card-footer">
-              <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-            </div>
-          </div>
-        </div>
+
 
         <!-- 
         <div class="col-lg-4 col-md-6 mb-4">
@@ -150,22 +167,7 @@
           </div>
         </div>
 
-        <div class="col-lg-4 col-md-6 mb-4">
-          <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-            <div class="card-body">
-              <h4 class="card-title">
-                <a href="#">Item Six</a>
-              </h4>
-              <h5>$24.99</h5>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-            </div>
-            <div class="card-footer">
-              <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-            </div>
-          </div>
-        </div>
-                  
+        
 -->
       </div>
       <!-- /.row CARDS-->
