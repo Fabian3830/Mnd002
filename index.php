54d1684ebabc8cae1c0e14f827d78b7f55537368
JAFTSIX <42688559+JAFTSIX@users.noreply.github.com>
2019-08-30 23:11:54 -0600

    necesito para ser Dios?

--- index.php
+++ index.php
@@ -23,13 +23,13 @@
         </ol>
         <div class="carousel-inner" role="listbox">
           <div class="carousel-item active">
-            <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide">
+            <img class="d-block img-fluid" src="SERVER\img\U.jpg" width="700"     alt="First slide">
           </div>
           <div class="carousel-item">
-            <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide">
+            <img class="d-block img-fluid" src="SERVER\img\B.jpg"  width="900"  alt="Second slide">
           </div>
           <div class="carousel-item">
-            <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide">
+            <img class="d-block img-fluid" src="SERVER\img\V.jpg" width="800"   alt="Third slide">
           </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
